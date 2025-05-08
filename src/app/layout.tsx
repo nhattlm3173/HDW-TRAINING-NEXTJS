7cfa7b6ecd25c73b5ebc5bf1aa9a14406194e8c8
nhattlm3173 <nhat.tlm3173@gmail.com>
2025-05-08 15:06:41 +0700

    Adds layout component with header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ToastProvider } from '@/component/Toast';
+import { Layout } from '@/component/Layout';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="mdl-js">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ToastProvider>{children}</ToastProvider>
+        <Layout>
+          <ToastProvider>{children}</ToastProvider>
+        </Layout>
       </body>
     </html>
   );
--- src/component/ConfirmModal/index.tsx
+++ src/component/ConfirmModal/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { ConfirmModalProps } from '../../type/confirm/IConfirm';
 import { useEffect, useState } from 'react';
 
