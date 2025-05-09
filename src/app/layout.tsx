ebd1f573205202d66f20ea324856f7ca117e78c7
nhattlm3173 <nhat.tlm3173@gmail.com>
2025-05-09 10:03:03 +0700

    feat: config redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ToastProvider } from '@/component/Toast';
 import { Layout } from '@/component/Layout';
+import { ReduxProvider } from '@/redux/provider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,9 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="mdl-js">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Layout>
-          <ToastProvider>{children}</ToastProvider>
-        </Layout>
+        <ReduxProvider>
+          <Layout>
+            <ToastProvider>{children}</ToastProvider>
+          </Layout>
+        </ReduxProvider>
       </body>
     </html>
   );
